92f76993937411944087c033fdf57983b7c6c8cd
dahdouhMouad <mouaddhd10@gmail.com>
2023-11-09 17:03:48 +0100

    Home tabview -wip-

--- Receipt ScannerSwiftUI/Home/HomeView.swift
+++ Receipt ScannerSwiftUI/Home/HomeView.swift
@@ -7,10 +7,39 @@
 
 import SwiftUI
 
+
+
 struct HomeView: View {
+    @State var selection: Int = 1
     var body: some View {
         NavigationStack {
-            Text("Home WIP")
+            TabView(selection: $selection) {
+                Text("Screen1")
+                    .tabItem {
+                        Label("Home", systemImage: "house")
+                    }
+                    .tag(1)
+                
+                Text("Screen2")
+                    .tabItem {
+                        Label("Scan", image: "2")
+                    }
+                    .tag(2)
+                
+                Text("Screen3")
+                    .tabItem {
+                        Label("Expenses", image: "3")
+                    }
+                    .tag(3)
+                
+                Text("Screen4")
+                    .tabItem {
+                        Label("Account", image: "4")
+                    }
+                    .tag(4)
+                
+            }
+            .tint(CustomColor.myBlue)
         }
         .navigationBarBackButtonHidden()
     }
