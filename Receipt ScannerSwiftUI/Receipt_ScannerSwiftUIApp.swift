b2f320b45078ada0798a5142d37901d6aa9f2c32
dahdouhMouad <mouaddhd10@gmail.com>
2023-11-09 12:09:21 +0100

    Initial Commit

---
+++ Receipt ScannerSwiftUI/Receipt_ScannerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Receipt_ScannerSwiftUIApp.swift
+//  Receipt ScannerSwiftUI
+//
+//  Created by Macbook Pro on 9/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Receipt_ScannerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
